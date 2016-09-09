164061b66863fc0e68ff04f5afd0a07ba3dc8d9e
rick.yin <ryin017@RYIN01724.us.nam.ad.pwcinternal.com>
2016-09-09 14:00:58 +0800

    [add some more features]

--- FluentEnglishDemo/app/src/main/java/com/example/ryin017/fluentenglishdemo/FluentScoreView.java
+++ FluentEnglishDemo/app/src/main/java/com/example/ryin017/fluentenglishdemo/FluentScoreView.java
@@ -6,24 +6,22 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.SweepGradient;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.animation.BounceInterpolator;
 import android.view.animation.LinearInterpolator;
+
+import java.text.DecimalFormat;
 
 /**
  * Created by ryin017 on 9/8/2016.
  */
 public class FluentScoreView extends View {
 
-    private String[] mScoreStr = new String[]{
-            "0", "起步",
-            "2000", "初级",
-            "4000", "全速",
-            "6000", "MAX",
-            "8000",
-    };
     // 默认宽高值
     private int defaultSize;
     // view宽度
@@ -38,6 +36,8 @@ public class FluentScoreView extends View {
     private int mMaxScore = 8000;
     //中间文本
     private Paint mTextPaint;
+    //中间灰色文本
+    private Paint mTextGrayPaint;
 
     //外层圆弧
     private Paint mOutArcPaint;
@@ -45,7 +45,6 @@ public class FluentScoreView extends View {
     //内环刻度
     private Paint mInnerArcPaint;
 
-    private RectF mOutRect;
 
     private RectF mInnerRect;
 
@@ -56,33 +55,40 @@ public class FluentScoreView extends View {
     private final static int defaultPadding = 30;
 
     //  圆环起始角度
-    private final static float mStartAngle = 165f;
+    private final static float mStartAngle = 160f;
 
-    // 圆环结束角度
-    private final static float mEndAngle = 210f;
+    // 圆环扫过角度
+    private final static float mSweepAngle = 220f;
     //小刻度画笔
     private Paint mSmallCalibrationPaint;
     //外环text
     private Paint mCalibrationTextPaint;
 
-    // 最外层渐变圆环画笔
-//    private Paint mGradientRingPaint;
 
     //进度圆环画笔
     private Paint mArcProgressPaint;
 
     // 指针全部进度
-    private float mTotalAngle = 210f;
+    private float mTotalAngle = 220f;
 
     // 指针当前进度
     private float mCurrentAngle = 0f;
 
+    private String mPercent = "";
+
+    private String[] mScoreStr = new String[]{
+            "0", "起步",
+            "2000", "初级",
+            "4000", "全速",
+            "6000", "MAX",
+            "8000",
+    };
 
     // 最外层圆环渐变色环颜色
     private final int[] mColors = new int[]{
-            0xFFFF0000,
-            0xFFFFD600,
-            0xFF00FF00
+            0xFF4CAF50,
+            0xFFFFC107,
+            0xFFCDDC39,
     };
 
     public FluentScoreView(Context context) {
@@ -100,7 +106,7 @@ public class FluentScoreView extends View {
 
     private void init() {
         defaultSize = dp2px(250);
-        arcDistance = dp2px(14);
+        arcDistance = dp2px(26);
 
         //外层圆弧画笔
         mOutArcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -121,6 +127,13 @@ public class FluentScoreView extends View {
         mTextPaint.setColor(Color.WHITE);
         mTextPaint.setTextAlign(Paint.Align.CENTER);
 
+
+        //中间灰色text
+        mTextGrayPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextGrayPaint.setColor(Color.WHITE);
+        mTextGrayPaint.setTextAlign(Paint.Align.CENTER);
+        mTextGrayPaint.setAlpha(130);
+
         //圆环小刻度画笔
         mSmallCalibrationPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mSmallCalibrationPaint.setStrokeWidth(1);
@@ -134,26 +147,21 @@ public class FluentScoreView extends View {
         mCalibrationTextPaint.setAlpha(130);
         mCalibrationTextPaint.setColor(Color.WHITE);
 
-//        mGradientRingPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-//        //设置圆环渐变色渲染
-//        mGradientRingPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
-//        float position[] = {0.1f, 0.3f, 0.8f};
-//        Shader mShader = new SweepGradient(width / 2, radius, mColors, position);
-//        mGradientRingPaint.setShader(mShader);
-//        mGradientRingPaint.setStrokeCap(Paint.Cap.ROUND);
-//        mGradientRingPaint.setStyle(Paint.Style.STROKE);
-//        mGradientRingPaint.setStrokeWidth(40);
-
-
         //外层进度画笔
         mArcProgressPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mArcProgressPaint.setStrokeWidth(40);
-        mArcProgressPaint.setColor(Color.BLUE);
+        mArcProgressPaint.setStrokeWidth(30);
+        mArcProgressPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+        float position[] = {0.1f, 0.2f, 0.3f};
+        Shader mShader = new SweepGradient(width / 2, radius, mColors, position);
+        mArcProgressPaint.setShader(mShader);
+        mArcProgressPaint.setStrokeCap(Paint.Cap.SQUARE);
         mArcProgressPaint.setStyle(Paint.Style.STROKE);
-        mArcProgressPaint.setStrokeCap(Paint.Cap.ROUND);
     }
 
     public void setScore(int score) {
+        float percent = (float) score / mMaxScore;
+        DecimalFormat df = new DecimalFormat("0.00%");
+        mPercent = "击败percent的用户".replace("percent", df.format(percent));
         mMaxScore = score;
         mTotalAngle = 140f;
         startAnim();
@@ -172,10 +180,6 @@ public class FluentScoreView extends View {
         height = h;
         radius = width / 2;
 
-
-        mOutRect = new RectF(
-                defaultPadding, defaultPadding,
-                width - defaultPadding, height - defaultPadding);
 
         mInnerRect = new RectF(
                 defaultPadding + arcDistance,
@@ -196,21 +200,20 @@ public class FluentScoreView extends View {
     }
 
     private void drawCenterText(Canvas canvas) {
-        mTextPaint.setTextSize(60);
-        canvas.drawText("口语力", radius, radius - 140, mTextPaint);
+        mTextGrayPaint.setTextSize(60);
+        canvas.drawText("口语力", radius, radius - 140, mTextGrayPaint);
 
         mTextPaint.setTextSize(200);
         mTextPaint.setStyle(Paint.Style.STROKE);
         canvas.drawText(String.valueOf(mMinScore), radius, radius + 70, mTextPaint);
 
-        mTextPaint.setTextSize(60);
+        mTextGrayPaint.setTextSize(60);
         mTextPaint.setStyle(Paint.Style.STROKE);
-        canvas.drawText("击败65%的用户", radius, radius + 180, mTextPaint);
+        canvas.drawText(mPercent, radius, radius + 180, mTextGrayPaint);
     }
 
     private void drawInnerArc(Canvas canvas) {
-        canvas.drawArc(mInnerRect, mStartAngle, mEndAngle, false, mInnerArcPaint);
-//        canvas.drawArc(mInnerRect, mStartAngle, mEndAngle, false, mGradientRingPaint);
+        canvas.drawArc(mInnerRect, mStartAngle, mSweepAngle, false, mInnerArcPaint);
     }
 
     /**
@@ -218,8 +221,7 @@ public class FluentScoreView extends View {
      *
      * @param canvas
      */
-    private void drawRingProgress(Canvas canvas)
-    {
+    private void drawRingProgress(Canvas canvas) {
 
         Path path = new Path();
         path.addArc(mInnerRect, mStartAngle, mCurrentAngle);
@@ -229,13 +231,14 @@ public class FluentScoreView extends View {
     private void drawSmallCalibration(Canvas canvas) {
         //旋转画布
         canvas.save();
-        canvas.rotate(-105, radius, radius);
+        canvas.rotate(-110, radius, radius);
         //计算刻度线的起点结束点
-        int startDst = (int) (defaultPadding - mOutArcPaint.getStrokeWidth() / 2 - 1);
+        int startDst = (int) (defaultPadding + mOutArcPaint.getStrokeWidth() / 2 + 30);
         int endDst = (int) (startDst + mOutArcPaint.getStrokeWidth());
-        for (int i = 0; i <= 208; i++) {
-            //每旋转6度绘制一个小刻度
-            canvas.drawLine(radius, startDst, radius, endDst, mSmallCalibrationPaint);
+        for (int i = 0; i <= 220; i++) {
+            if (i % 55 != 0) {
+                canvas.drawLine(radius, startDst, radius, endDst, mSmallCalibrationPaint);
+            }
             canvas.rotate(1, radius, radius);
         }
         canvas.restore();
@@ -243,13 +246,13 @@ public class FluentScoreView extends View {
 
     private void drawBigCalibration(Canvas canvas) {
         canvas.save();
-        canvas.rotate(-105, radius, radius);
-        int startDst = (int) (defaultPadding / 2 - mOutArcPaint.getStrokeWidth() / 2);
+        canvas.rotate(-110, radius, radius);
+        int startDst = (int) (defaultPadding - mOutArcPaint.getStrokeWidth() / 2 + 20);
         int endDst = (int) (startDst + mOutArcPaint.getStrokeWidth() + defaultPadding);
         for (int i = 0; i <= 4; i++) {
-            //每旋转6度绘制一个小刻度
-            canvas.drawLine(radius, startDst, radius, endDst, mSmallCalibrationPaint);
-            canvas.rotate(52f, radius, radius);
+            //每旋转55度绘制一个大刻度
+            canvas.drawLine(radius, startDst, radius + 1, endDst, mSmallCalibrationPaint);
+            canvas.rotate(55f, radius, radius);
         }
         canvas.restore();
     }
@@ -257,24 +260,23 @@ public class FluentScoreView extends View {
 
     private void drawCalibrationAndText(Canvas canvas) {
         canvas.save();
-        canvas.rotate(-105, radius, radius);
+        canvas.rotate(-110, radius, radius);
         int startDst = (int) (defaultPadding + arcDistance - mOutArcPaint.getStrokeWidth() / 2 - 1);
         int endDst = (int) (startDst + mOutArcPaint.getStrokeWidth());
         //刻度旋转的角度
-        int rotateAngle = 210 / 8;
+        int rotateAngle = 220 / 8;
         for (int i = 0; i <= 8; i++) {
             // 测量文本的长度
             float textLen = mCalibrationTextPaint.measureText(mScoreStr[i]);
-            canvas.drawText(mScoreStr[i], radius - textLen / 2, endDst - 80, mCalibrationTextPaint);
+            canvas.drawText(mScoreStr[i], radius - textLen / 2 + 6, endDst - 100, mCalibrationTextPaint);
             canvas.rotate(rotateAngle, radius, radius);
         }
-
         canvas.restore();
     }
 
     private void startAnim() {
         ValueAnimator scoreAnim = ValueAnimator.ofInt(mMinScore, mMaxScore);
-        scoreAnim.setDuration(2500);
+        scoreAnim.setDuration(2000);
         scoreAnim.setInterpolator(new LinearInterpolator());
         scoreAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
@@ -286,14 +288,12 @@ public class FluentScoreView extends View {
         scoreAnim.start();
 
         ValueAnimator mAngleAnim = ValueAnimator.ofFloat(mCurrentAngle, mTotalAngle);
-        mAngleAnim.setInterpolator(new BounceInterpolator());
-        mAngleAnim.setDuration(2500);
-        mAngleAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
-        {
+        mAngleAnim.setInterpolator(new LinearInterpolator());
+        mAngleAnim.setDuration(2000);
+        mAngleAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
             @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator)
-            {
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
 
                 mCurrentAngle = (float) valueAnimator.getAnimatedValue();
                 postInvalidate();
